d51c9b31fffd4a68fdaf259c2e63677b3702cccf
Prachi-215 <prachirakshak02@gmail.com>
2025-07-25 00:08:32 +0530

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Personal Portfolio Website",
+  base:"/Personal Portfolio Website/",
   css: {
     modules: {
       localsConvention: "camelCase"
